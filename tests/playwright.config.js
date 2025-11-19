51670c3851d1bd95de4932253f026016e8514eed
Jorge Balderas <jorgerbf@gmail.com>
2025-11-19 17:10:49 -0500

    increased worker count

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 6 : undefined,
   reporter: [
     ['html'],
     ['junit', { outputFile: 'test-results/junit.xml' }],
