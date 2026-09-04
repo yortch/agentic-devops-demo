35da64d39d00c54087198914b85aa8e2b63bdfe0
Jorge Balderas <jorgerbf@gmail.com>
2026-09-04 15:51:36 -0400

    refactor: simplify import statements in BianApiClientConfig and BillingDto

--- backend/src/main/java/com/threeriversbank/client/BianApiClientConfig.java
+++ backend/src/main/java/com/threeriversbank/client/BianApiClientConfig.java
@@ -2,8 +2,7 @@ package com.threeriversbank.client;
 
 import feign.Logger;
 import feign.RequestInterceptor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
 public class BianApiClientConfig {
--- backend/src/main/java/com/threeriversbank/model/dto/BillingDto.java
+++ backend/src/main/java/com/threeriversbank/model/dto/BillingDto.java
@@ -1,9 +1,6 @@
 package com.threeriversbank.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
