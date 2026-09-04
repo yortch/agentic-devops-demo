d29b06589bb8796f1fd95e7391611fae90929792
Jorge Balderas <jorgerbf@gmail.com>
2026-09-04 14:47:34 -0400

    refactor: remove wildcard imports and specify individual DTO and entity imports

--- backend/src/test/java/com/threeriversbank/controller/CreditCardControllerTest.java
+++ backend/src/test/java/com/threeriversbank/controller/CreditCardControllerTest.java
@@ -15,7 +15,8 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CreditCardController.class)
 class CreditCardControllerTest {
